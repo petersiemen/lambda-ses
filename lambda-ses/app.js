613d09cb5337c0143a2f6008cab923a34b10f939
Peter Siemen <mail@petersiemen.net>
2020-04-01 11:57:16 +0200

    init2

---
+++ lambda-ses/app.js
@@ -0,0 +1,59 @@
+var aws = require('aws-sdk');
+var ses = new aws.SES({region: 'eu-central-1'});
+
+exports.handler = (event, context, callback) => {
+
+    var currentTime = new Date();
+    var email = process.env.EMAIL;
+    console.log('Sending e-mail from:', email);
+    console.log(event);
+    var body = JSON.parse(event.body);
+    console.log('body:', body);
+    // var subject = body.subject;
+    // var message = body.message;
+    //
+    var params = {
+        Destination: {
+            ToAddresses: [email]
+        },
+        Message: {
+            Body: {
+                Text: {
+                    Data: "Test"
+                }
+            },
+            Subject: {
+                Data: "bla blah"
+            }
+        },
+        Source: email
+    };
+
+    callback(null, {
+        statusCode: '200',
+        body: JSON.stringify({
+            message: 'The time is: ' + currentTime.toString(),
+            // err: err,
+            // data: data
+        })
+    });
+    //
+    // ses.sendEmail(params, function (err, data) {
+    //     callback(null, {
+    //         statusCode: '200',
+    //         body: JSON.stringify({
+    //             message: 'The time is: ' + currentTime.toString(),
+    //             err: err,
+    //             data: data
+    //         })
+    //     });
+    //
+    //     if (err) {
+    //         console.log(err);
+    //         context.fail(err);
+    //     } else {
+    //         console.log(data);
+    //         context.succeed(event);
+    //     }
+    // });
+};
